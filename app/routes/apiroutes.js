41fccc4a3b1ea7cfc754d2fab0cb1ab95ca95a74
Ekscursionist <larakek@gmail.com>
2018-09-07 16:48:56 -0400

    continuing writing apiroutes. testing one book. Works!

--- app/routes/apiroutes.js
+++ app/routes/apiroutes.js
@@ -12,6 +12,7 @@ var Book = require("../models/book.js");
 // --------------------------
 module.exports = function(app) {
 
+    // ----- GET ALL BOOKS
     app.get("/api/all", function(req, res) {
         Book.findAll({}).then(function(results){
             res.json(results);
@@ -21,6 +22,21 @@ module.exports = function(app) {
     // -- looking in models/book.js via sequelize.define("book") in the database),
     // -- then res.json (i.e, display in JSON) everything it finds (i.e, results).
 
+    // ----- GET JUST ONE BOOK
+    app.get("/api/:book", function(req, res) {
+        if (req.params.book) {
+            Book.findAll({
+                where: {
+                    title: req.params.book
+                }
+            }).then(function(results){
+                res.json(results);
+            });
+        }
+    });
+    // -- On the API Route, we put a parameter, :book. 
+    // -- IF that exists, THEN findAll ONLY WHERE TITLE: that book.
+    // -- (Then display the results in JSON format :D )
 
 
 
