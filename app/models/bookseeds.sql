7d8979a39cb5f21397c5edb5cb65a12066892fbd
Ekscursionist <larakek@gmail.com>
2018-09-06 20:12:56 -0400

    adding more seeds

--- app/models/bookseeds.sql
+++ app/models/bookseeds.sql
@@ -1,14 +1,32 @@
 INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
-VALUES ("Interstellar Pig", "William", "Sleator", "science fiction", "3", "August", "2018", "false", "n/a");
+VALUES ("The Tempest", "William", "Shakespeare", "classic", "4", "July", "2018", "false", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Starship Troopers", "Robert", "Heinlein", "science fiction", "3.2", "July", "2018", "false", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Legion and The Emperor's Soul", "Brandon", "Sanderson", "fantasy", "4", "July", "2018", "false", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Mistborn", "Brandon", "Sanderson", "fantasy", "4", "August", "2018", "true", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("The Well of Ascension", "Brandon", "Sanderson", "fantasy", "4", "August", "2018", "true", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("The Hero of Ages", "Brandon", "Sanderson", "fantasy", "4", "August", "2018", "true", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Elantris", "Brandon", "Sanderson", "fantasy", "3.7", "August", "2018", "true", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("How The Other Half Lives", "Jacob", "Riis", "historical journalism", "4", "August", "2018", "false", "n/a");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Tempests and Slaughter", "Tamora", "Pierce", "YA fantasy", "3.8", "August", "2018", "false", "not as good as Pierce's other works.  Not terrible, but not my favorite.  She's learned as a writer, though.");
+
+INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
+VALUES ("Interstellar Pig", "William", "Sleator", "science fiction", "3.5", "August", "2018", "false", "n/a");
 
 INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
 VALUES ("The Knife of Never Letting Go", "Patrick", "Ness", "YA science fiction", "5", "September", "2018", "true", "very harsh, but very well written.");
-
-INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
-VALUES ("Interstellar Pig", "William", "Sleator", "science fiction", "3", "August", "2018", "false", "n/a");
-
-INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
-VALUES ("Interstellar Pig", "William", "Sleator", "science fiction", "3", "August", "2018", "false", "n/a");
-
-INSERT INTO books (title, author_firstname, author_lastname, genre, rating, month_read, year_read, reread, notes)
-VALUES ("Interstellar Pig", "William", "Sleator", "science fiction", "3", "August", "2018", "false", "n/a");
